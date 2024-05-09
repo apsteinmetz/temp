68aff259c72175cdb78fc82f9fd7b8a430ef22e9
Art Steinmetz <apsteinmetz@yahoo.com>
2024-05-09 15:58:10 -0400

    extract table from sunpower pdf

--- solar/solar.R
+++ solar/solar.R
@@ -41,18 +41,42 @@ powerwall |>
   summarise(across(where(is.numeric),\(x) sum(x,na.rm = TRUE)))
 
 # extract table from pdf
-list.files("data", full.names = TRUE) |> 
+pdf_fnames <- list.files("data", full.names = TRUE) |> 
   enframe() |> 
   filter(str_detect(value,"Resi")) |> 
-  pull(value) |> 
-  pdftools::pdf_text()
+  pull(value)
 
-|> 
-  str_split("\n") |> 
-  unlist() |> 
-  as_tibble() |> 
-  filter(str_detect(value,"[0-9]")) |> 
-  separate(value,into = c("date","time","temp","humidity","wind_speed","wind_dir"),sep = "\\s+") |> 
-  mutate(date = as.Date(date),time = hms::as_hms(time)) |> 
-  select(date,time,temp,humidity,wind_speed,wind_dir) |> 
-  arrange(date,time)
+
+# determine the row boundaries of table
+locate_row <- function(pdf_text,tag){
+  mutate(pdf_text,row_number = row_number()) |>
+    filter(str_detect(value,tag)) |> 
+    pull(row_number) 
+}
+
+# make table from pdf text
+make_table <- function(pdf_fname) {
+  pdf_df <- pdftools::pdf_text(pdf_fname)  |>
+    str_split("\n") |>
+    unlist() |>
+    as_tibble()
+  table_start <- locate_row(pdf_df, "Day") + 1
+  # "Refer" shows up twice. take second instance
+  table_end <- locate_row(pdf_df, "Refer")[2] - 1
+  energy_table <- pdf_df[table_start:table_end, ] |>
+    # separate temp2 into columns where separator is two or more spaces
+    separate(
+      value,
+      into = c("dummy", "date", "solar_energy", "home", "peak_gen_kw"),
+      sep = "\\s{2,}"
+    ) |>
+    select(-dummy) |>
+    na.omit() |>
+    # convert date column in format "mmm dd, yyyy" to date type
+    mutate(date = as.Date(date, "%b %d, %Y")) |>
+    # convert other columns to numeric type
+    mutate(across(where(is.character), as.numeric))
+  return(energy_table)
+}
+
+make_table(temp)
